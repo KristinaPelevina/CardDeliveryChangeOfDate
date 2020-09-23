2f12cfef77809568dce8c4e09c309e3c3c0766ad
Kristina Pelevina <christya_13.07@mail.ru>
2020-09-23 15:19:37 +0600

    Added java classes CardDeliveryApplicationTest.java, DataGenerator.java

---
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -0,0 +1,44 @@
+package ru.netology.web;
+
+import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+@Data
+@NoArgsConstructor
+public class DataGenerator {
+
+    public static String getRandomCity() {
+        List<String> list = Arrays.asList("Екатеринбург", "Улан-Удэ", "Красноярск", "Санкт-Петербург", "Москва");
+        Random rand = new Random();
+        String randomElement = list.get(rand.nextInt(list.size()));
+        return randomElement;
+    }
+
+    public static String getCorrectDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String getIncorrectDate() {
+        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return today;
+    }
+
+    public static String getRandomName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String getRandomPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
